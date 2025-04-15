8afae9353a9b849d9291c2d19ce9e3cc7da8bf61
Michael Nobles <Mnobles33@gmail.com>
2025-04-15 08:44:38 -0700

    Test commit

--- api/server.js
+++ api/server.js
@@ -2,21 +2,21 @@
 const express = require('express');
 const server = express();
 
-const projectRouter = require('./project/router');
-const resourceRouter = require('./resource/router');
-const taskRouter = require('./task/router');
+// const projectRouter = require('./project/router');
+// const resourceRouter = require('./resource/router');
+// const taskRouter = require('./task/router');
 
-server.use(express.json());
-server.use('/api/project', projectRouter);
-server.use('/api/resource', resourceRouter);
-server.use('/api/task', taskRouter);
+// server.use(express.json());
+// server.use('/api/project', projectRouter);
+// server.use('/api/resource', resourceRouter);
+// server.use('/api/task', taskRouter);
 
-server.get('*', (req, res, next) => { //eslint-disable-line
-    res.status(400).json({ message: "Nothing to see here..." });
-})
+// server.get('*', (req, res, next) => { //eslint-disable-line
+//     res.status(400).json({ message: "Nothing to see here..." });
+// })
 
-server.use((error, req, res, next) => { //eslint-disable-line
-    res.status(500).json({ message: error.message });
-})
+// server.use((error, req, res, next) => { //eslint-disable-line
+//     res.status(500).json({ message: error.message });
+// })
 
 module.exports = server
