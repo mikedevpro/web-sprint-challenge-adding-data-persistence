15127b1efa19237a03d1645f19d13773ab2ae804
Michael Nobles <Mnobles33@gmail.com>
2025-04-15 09:04:58 -0700

    next commit

--- api/server.js
+++ api/server.js
@@ -1,15 +1,14 @@
 // build your server here and require it from index.js
 const express = require('express');
+const projectsRouter = require('./project/router');
+const resourcesRouter = require('./resource/router');
+const tasksRouter = require('./task/router');
 const server = express();
 
-const projectRouter = require('./project/router');
-const resourceRouter = require('./resource/router');
-const taskRouter = require('./task/router');
-
 server.use(express.json());
-server.use('/api/project', projectRouter);
-server.use('/api/resource', resourceRouter);
-server.use('/api/task', taskRouter);
+server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
+server.use('/api/tasks', tasksRouter);
 
 // server.get('*', (req, res, next) => { //eslint-disable-line
 //     res.status(400).json({ message: "Nothing to see here..." });
@@ -18,5 +17,6 @@ server.use('/api/task', taskRouter);
 // server.use((error, req, res, next) => { //eslint-disable-line
 //     res.status(500).json({ message: error.message });
 // })
+
 
 module.exports = server
