ff5e3f21ace844cff0958c3a15031fea9e532cdb
Michael Nobles <Mnobles33@gmail.com>
2025-04-15 08:51:12 -0700

    Next commit

--- api/server.js
+++ api/server.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const server = express();
 
-// const projectRouter = require('./project/router');
-// const resourceRouter = require('./resource/router');
-// const taskRouter = require('./task/router');
+const projectRouter = require('./project/router');
+const resourceRouter = require('./resource/router');
+const taskRouter = require('./task/router');
 
-// server.use(express.json());
-// server.use('/api/project', projectRouter);
-// server.use('/api/resource', resourceRouter);
-// server.use('/api/task', taskRouter);
+server.use(express.json());
+server.use('/api/project', projectRouter);
+server.use('/api/resource', resourceRouter);
+server.use('/api/task', taskRouter);
 
 // server.get('*', (req, res, next) => { //eslint-disable-line
 //     res.status(400).json({ message: "Nothing to see here..." });
