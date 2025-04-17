8a49479d55da6219b6ae8e633a8b9a7cfb5b3551
Michael Nobles <Mnobles33@gmail.com>
2025-04-17 14:22:38 -0700

    next commit

--- api/task/router.js
+++ api/task/router.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const router = express.Router();
 const Task = require('../task/model')
+const db = require('../../data/dbConfig')
 
 // const {
 //   getTasks,
@@ -18,15 +19,43 @@ router.get("/", (req, res, next) => {
     .catch(next);
 });
 
-router.post('/', (req, res, next) => {
-  const newProject = req.body
+// router.post('/', (req, res, next) => {
+//   const newProject = req.body
 
-  Task.insertTask(newProject)
-    .then(addProject => {
-      res.status(201).json(addProject);
-    })
-    .catch(next)
-})
+//   Task.insertTask(newProject)
+//     .then(addProject => {
+//       res.status(201).json(addProject);
+//     })
+//     .catch(next)
+// })
+
+router.post("/", async (req, res) => {
+  try {
+    const { task_description, task_notes, task_completed, project_id } = req.body;
+
+    if (!task_description) {
+      return res.status(400).json({ message: "Task description is required." });
+    }
+    if(!project_id) {
+      return res.status(400).json({ message: "Project ID is required." });
+    }
+    const [task_id] = await db("tasks")
+      .insert({
+        task_description,
+        task_notes,
+        task_completed: task_completed ? 1 : 0,
+        project_id,
+    });
+
+    const task = await db("tasks").where({ task_id }).first();
+
+    task.task_completed = !!task.task_completed;
+
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 router.use((err, req, res, next) => { //eslint-disable-line
   res.status(err.status || 500).json({
@@ -35,35 +64,6 @@ router.use((err, req, res, next) => { //eslint-disable-line
     stack: err.stack,
   });
 });
-
-// router.post("/", async (req, res) => {
-//   try {
-//     const { task_description, task_notes, task_completed, project_id } = req.body;
-
-//     if (!task_description) {
-//       return res.status(400).json({ message: "Task description is required." });
-//     }
-//     if(!project_id) {
-//       return res.status(400).json({ message: "Project ID is required." });
-//     }
-//     const [task_id] = await db("tasks")
-//       .insert({
-//         task_description,
-//         task_notes,
-//         task_completed: task_completed ? 1 : 0,
-//         project_id,
-//     });
-
-//     const task = await db("tasks").where({ task_id }).first();
-
-//     task.task_completed = !!task.task_completed;
-
-//     res.status(201).json(task);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
 
 
 
